5706b68abc8c8abe7e35649f23f7417be4e0e6d9
Ashutosh Anand <ashutosh.bsc@gmail.com>
2019-06-13 22:26:48 +0530

    Updates

---
+++ precache-manifest.39e68fb22ec4a4aa47259735687294d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e9b96003537f7cddb382fba6533bfa",
+    "url": "/simple-file-folder/index.html"
+  },
+  {
+    "revision": "9b43ec799aa8d1e68f84",
+    "url": "/simple-file-folder/static/css/main.0f1ee1a7.chunk.css"
+  },
+  {
+    "revision": "e98d80cd20b585ea7688",
+    "url": "/simple-file-folder/static/js/2.38451c68.chunk.js"
+  },
+  {
+    "revision": "9b43ec799aa8d1e68f84",
+    "url": "/simple-file-folder/static/js/main.a034f083.chunk.js"
+  },
+  {
+    "revision": "a303cb56f1f0fd959a0a",
+    "url": "/simple-file-folder/static/js/runtime~main.b438b0a3.js"
+  }
+]);
